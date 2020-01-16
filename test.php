c5120c778f284846adec936aef63dd55f094a0a6
kis龍 <pifeifei1989@qq.com>
2020-01-16 16:43:38 +0800

    update

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+$adTime = 123;
+$id = 11;
+$adPositionDetail = [];
+$data = [];
+if ($adTime > 0
+    && $id === $adPositionDetail['id']
+    && $adTime >= $data['payment_time'] && $adTime <= $data['payment_time'] + 86400
+) {
+    $data['is_start_ad'] = 1;
+}
+
+
+function test_test()
+{
+    echo 'da';
+}
+
+
+test_test();
